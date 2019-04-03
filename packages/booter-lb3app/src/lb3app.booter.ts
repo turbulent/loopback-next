3c878d1d80ffb6713cf527aac69edd970208883d
Nora <nora.abdelgadir@ibm.com>
2019-04-03 10:54:11 -0400

    chore(booter-lb3app): set up package

---
+++ packages/booter-lb3app/src/lb3app.booter.ts
@@ -0,0 +1,8 @@
+// Copyright IBM Corp. 2019. All Rights Reserved.
+// Node module: @loopback/booter-lb3app
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {Booter} from '@loopback/boot';
+
+export class Lb3AppBooter implements Booter {}
